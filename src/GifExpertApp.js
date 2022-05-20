a382342dc787778de7041a408f36f7ea646358e0
Johnny Quezada <jhonnyquezada1997@gmail.com>
2022-05-19 20:28:40 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch Man']);
+
+  // const handleAdd = () => {
+    //     setCategories( [...categories,'HunterXHunter'] );
+    // }
+
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories = { setCategories } />
+            <hr/>
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid category={ category }
+                        key = { category } />
+                    ))
+                }
+            </ol>
+        </>
+  )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+ export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=yrEYyg03Klseb2Iag8Ynla0sBn8QToXv`
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    
+    return gifs;
+}
